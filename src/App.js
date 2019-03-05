5eddcc21f7209c0c77755dd46bbb2fec59fbcebe
Shosuke Doi <doiverson.canada@gmail.com>
2019-03-04 19:48:55 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
+// import { Admin, Resource, ListGuesser } from 'react-admin';
+// import { Admin, Resource, EditGuesser } from 'react-admin';
+import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
+import Dashboard from './Dashboard';
+import authProvider from './authProvider';
 import jsonServerProvider from 'ra-data-json-server';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
-    <Admin dataProvider={dataProvider}>
-      <Resource name="users" list={UserList} />
+    <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+      {/*<Resource name="posts" list={ListGuesser} />*/}
+      {/*<Resource name="posts" list={PostList} />*/}
+      <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+      <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
 );
 
